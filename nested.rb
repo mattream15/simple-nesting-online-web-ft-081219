c64cda74560eaffbb2bb6ece9f945aa8f9f97e56
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-27 15:59:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash => {:yukihiro_matsumoto}
+programmer_hash => {:yukihiro_matsumoto => {:known_for, :languages}}
 programmer_hash[:yukihiro_matsumoto]["known_for"] = "Unix"
 programmer_hash[:yukihiro_matsumoto]["languages"] = ["LISP", "C"]
   programmer_hash
