1009bd926f3de929a4e4645d3cc260084f885c4f
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-27 16:44:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,6 @@ def adding_to_dennis
         }
  		}
  	dennis_new_info = "Assembly"
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages] = dennis_new_info
 programmer_hash
 end
